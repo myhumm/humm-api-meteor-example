20c88f22bec707994b9e82966fe034267a314101
MoeBadr <mohamedbadr22@gmail.com>
2015-11-30 20:28:58 +0000

    almost

---
+++ server/accessViaCodeGrant.js
@@ -0,0 +1,51 @@
+Meteor.methods({
+
+    /**
+     * Get user using the code
+     * @param code
+     */
+    getMe: function getMe(code) {
+        /**
+         * this will allow the next available DDP message to process without waiting for the current method.
+         */
+        this.unblock();
+        // init humm
+        humm.init({
+            client_id: '56570bacae8c5087411778a3',
+            client_secret: 'CdNX3TcLc/OF3k2oIogwlBi/rCZOP0LSfLxrRjoX5EA='
+        });
+
+        return Async.runSync(function(done) {
+            authAndFetchUser(code, done);
+        });
+    }
+});
+
+
+
+function authAndFetchUser(code, done){
+    //request access token with code
+    humm.accessViaCodeGrant(code, function(authErr, authRes) {
+        console.log('------------- accessViaCodeGrant complete -------------');
+        console.log('authErr');
+        console.log(authErr);
+        console.log('AuthRes');
+        console.log(authRes);
+
+        if(!authErr && authRes) {
+
+            //set token before request
+            humm.setAccessToken(authRes.access_token);
+            //get current user
+            humm.users.me(function(meErr, meRes){
+                console.log('--------------------- users.me()----------');
+                console.log(meErr);
+                console.log(meRes);
+                //send response back
+                done(null, { auth: authRes,  me: meRes });
+            });
+        }else {
+            done(authErr, null);
+        }
+    });
+}
